3e6ff5214052bc2f6da9890fa4536d4e5b913511
TicoNotebook <tgafa@cesar.school>
2024-05-08 20:59:02 -0300

    Refactor dynamic allocation code
    This commit adds a new file `alocacao dinamica.c` that demonstrates dynamic memory allocation in C. The code prompts the user to enter the size of an array, allocates memory for the array using `malloc`, and then reads and prints the array elements. Finally, the allocated memory is freed using `free`.
    
    Note: The commit message follows the established convention of providing a concise and descriptive summary of the code changes.

---
+++ Fundamentos/alocacao dinamica.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+    int *arr;
+    int len, i;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &len);
+    arr = (int*)malloc(len*(sizeof(int)));
+
+    for(i = 0; i<len; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Vetor: ");
+    for(i = 0; i<len; i++){
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+
+    return 0;
+}
