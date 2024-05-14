125ab34e3b7de1ed08d1b0897f52dc9eb0343554
TicoNotebook <tgafa@cesar.school>
2024-05-14 09:07:56 -0300

    refactor: Improve linked list insertion functions

--- Lista 06 - Listas Encadeadas/tgafa/q2_tgafa.c
+++ Lista 06 - Listas Encadeadas/tgafa/q2_tgafa.c
@@ -30,31 +30,42 @@ void insertNode(Node **head, char *name){
     }
 }
 
-void insertBefore(Node **head, char *name, char *target){
-    Node *newNode = createNode(name);
-    if (*head == NULL){
-        *head = newNode;
+void insertBefore(Node **head, Node *newList, char *target){
+    if (*head == NULL || newList == NULL){
+        return;
+    }
+
+    Node *current = *head;
+    Node *prev = NULL;
+
+    while (current != NULL && strcmp(current->name, target) != 0){
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == NULL){
+        return;
+    }
+
+    if (prev == NULL){
+        Node *lastNode = newList;
+        while (lastNode->next != NULL){
+            lastNode = lastNode->next;
+        }
+        lastNode->next = *head;
+        *head = newList;
     }
     else{
-        Node *current = *head;
-        Node *prev = NULL;
-        while (current != NULL && strcmp(current->name, target) != 0){
-            prev = current;
-            current = current->next;
+        Node *lastNode = newList;
+        while (lastNode->next != NULL){
+            lastNode = lastNode->next;
         }
-        if (prev == NULL){
-            newNode->next = *head;
-            *head = newNode;
-        }
-        else{
-            prev->next = newNode;
-            newNode->next = current;
-        }
+        lastNode->next = current;
+        prev->next = newList;
     }
 }
 
-void printList(Node *head)
-{
+void printList(Node *head){
     Node *current = head;
     while (current != NULL){
         printf("%s ", current->name);
@@ -82,35 +93,38 @@ int main(){
     fgets(input, MAX_NAME_LENGTH, stdin);
     char *token = strtok(input, " ");
     while (token != NULL){
-        insertNode(&friendsList, token);
-        token = strtok(NULL, " ");
-    }
+    char *name = strdup(token);  // Create a copy of the token
+    insertNode(&friendsList, name);
+    token = strtok(NULL, " ");
+}
 
     fgets(input, MAX_NAME_LENGTH, stdin);
     token = strtok(input, " ");
     while (token != NULL){
-        insertNode(&newList, token);
-        token = strtok(NULL, " ");
-    }
+    char *name = strdup(token);  // Create a copy of the token
+    insertNode(&newList, name);
+    token = strtok(NULL, " ");
+}
 
     fgets(target, MAX_NAME_LENGTH, stdin);
     target[strcspn(target, "\n")] = '\0';
 
     if (strcmp(target, "nao") != 0){
-        insertBefore(&friendsList, newList->name, target);
-    }
+    insertBefore(&friendsList, newList, target);
+}
     else{
-        Node *current = newList;
-        while (current != NULL){
-            insertNode(&friendsList, current->name);
-            current = current->next;
-        }
+    Node *current = newList;
+    while (current != NULL){
+        insertNode(&friendsList, current->name);
+        current = current->next;
     }
+}
 
-    printList(friendsList);
+printList(friendsList);
 
-    freeList(friendsList);
-    freeList(newList);
+
+freeList(friendsList);
+freeList(newList);
 
     return 0;
 }
