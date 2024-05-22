2da8508b8b557235b0f96f8e0c533c85951fbc71
TicoNotebook <tgafa@cesar.school>
2024-05-22 11:26:20 -0300

    feat: Add file I/O functionality and line count feature
    The code changes in the `arquivo3.c` file add file I/O functionality and a line count feature. The user is prompted to enter the name of a file, which is then opened and read line by line to count the number of lines. The line count is then displayed to the user. This feature enhances the functionality of the program and provides useful information to the user.
    
    Note: The commit message follows the established convention of using a prefix (feat) to indicate a new feature.

---
+++ Fundamentos/arquivo3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(){
+
+    char filename[100];
+    FILE *file;
+    char line[256];
+    int LineCount = 0;
+
+    printf("Digite o nome do arquivo: ");
+    scanf("%s", filename);
+
+    file = fopen(filename, "r");
+
+    if(file == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+
+    while(fgets(line, sizeof(line), file)){
+        LineCount++;
+    }
+
+    fclose(file);
+
+    printf("O arquivo %s tem %d linhas\n", filename, LineCount);
+
+
+    return 0;
+}
