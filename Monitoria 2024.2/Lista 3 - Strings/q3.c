08980edee607d637cf4caa3ff88ebf12da31f73f
ticogafa <tgafa@cesar.school>
2024-09-17 15:17:25 -0300

    Increase size of character arrays for storing surnames in q3.c and update .gitignore

--- Monitoria 2024.2/Lista 3 - Strings/q3.c
+++ Monitoria 2024.2/Lista 3 - Strings/q3.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-// Função para encontrar o maior prefixo comum entre duas strings
-int maior_prefixo_comum(char *str1, char *str2) {
-    int i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0' && str1[i] == str2[i]) {
-        i++;
+#define ALFABETO 26
+#define MAX_NODES 1000000
+
+typedef struct Node {
+    struct Node* filhos[ALFABETO];
+    int contagem;
+} Node;
+
+Node node_pool[MAX_NODES];
+int node_count = 0;
+
+Node* iniciar() {
+    Node* no = &node_pool[node_count++];
+    no->contagem = 0;
+    for (int i = 0; i < ALFABETO; i++) {
+        no->filhos[i] = NULL;
     }
-    return i;
+    return no;
+}
+
+void adicionar_palavra(Node* raiz, char* palavra) {
+    Node* atual = raiz;
+    for (int k = 0; palavra[k] != '\0'; k++) {
+        int letra = palavra[k] - 'A';
+        if (!atual->filhos[letra]) {
+            atual->filhos[letra] = iniciar();
+        }
+        atual = atual->filhos[letra];
+        atual->contagem++;
+    }
+}
+
+void contar_prefixos(Node* no1, Node* no2, int* resposta) {
+    if (!no1 || !no2) return;
+    *resposta += (no1->contagem < no2->contagem) ? no1->contagem : no2->contagem;
+    for (int i = 0; i < ALFABETO; i++) {
+        contar_prefixos(no1->filhos[i], no2->filhos[i], resposta);
+    }
+}
+
+void liberar(Node* no) {
+    // Não é mais necessário liberar nós individualmente
+}
+
+void resolver(int total_palavras) {
+    Node* raiz1 = iniciar();
+    Node* raiz2 = iniciar();
+    int resposta = 0;
+
+    for (int i = 0; i < total_palavras; i++) {
+        char palavra[100];
+        scanf("%s", palavra);
+        adicionar_palavra(raiz1, palavra);
+    }
+
+    for (int i = 0; i < total_palavras; i++) {
+        char palavra[100];
+        scanf("%s", palavra);
+        adicionar_palavra(raiz2, palavra);
+    }
+
+    contar_prefixos(raiz1, raiz2, &resposta);
+    printf("%d\n", resposta);
+
+    // Não é mais necessário liberar nós individualmente
 }
 
 int main() {
-    int N;
-    
+    int total_palavras;
     while (1) {
-        scanf("%d", &N);
-        if (N == -1) break;
-
-        char sub18[N][101], sub21[N][101];
-        
-        for (int i = 0; i < N; i++) {
-            scanf("%s", sub18[i]);
-        }
-        for (int i = 0; i < N; i++) {
-            scanf("%s", sub21[i]);
-        }
-
-        int max_letras = 0;
-        for (int i = 0; i < N; i++) {
-            int max_prefixo = 0;
-            for (int j = 0; j < N; j++) {
-                int prefixo = maior_prefixo_comum(sub18[i], sub21[j]);
-                if (prefixo > max_prefixo) {
-                    max_prefixo = prefixo;
-                }
-            }
-            max_letras += max_prefixo;
-        }
-
-        printf("%d\n", max_letras);
+        scanf("%d", &total_palavras);
+        if (total_palavras == -1) break;
+        resolver(total_palavras);
     }
-
     return 0;
 }
